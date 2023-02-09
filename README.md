1230fbf195c3093eb2fe412700e567cffab46cab
luoxin <luoxin@wunsun.com>
2023-02-09 15:38:44 +0800

    init database

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # go-utils
 
-a simple go project tools package
+一个简单的Go项目工具包
+
+## 配置
+> 用法
+```
+    config.Init(ENV)
+    stringValue := config.GetViper.GetString(KEY)
+```
+## 数据库
+> 用法
+
+```
+	database.Init()
+	baseDb := database.Get("base")
+```
+## 缓存
---
+++ database/database.go
@@ -0,0 +1,46 @@
+package database
+
+import (
+	"bolo/go-utils/config"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+var databases = make(map[string]*gorm.DB)
+
+func Init() {
+	conf := config.GetViper()
+	dbConfigs := conf.GetStringMap("database")
+	for key, value := range dbConfigs {
+		dbConfig := value.(map[string]interface{})
+
+		var host = dbConfig["host"].(string)
+		var port = dbConfig["port"].(string)
+		var dbname = dbConfig["dbname"].(string)
+		var username = dbConfig["username"].(string)
+		var password = dbConfig["password"].(string)
+		var prefix = dbConfig["prefix"].(string)
+		
+		dsn := username + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbname + "?charset=utf8mb4&parseTime=True&loc=Local"
+		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+			NamingStrategy: schema.NamingStrategy{
+				TablePrefix: prefix,
+			},
+		})
+		if err != nil {
+			log.Fatal("数据库连接失败！")
+		}
+		databases[key] = db
+	}
+}
+
+func Get(key string) *gorm.DB {
+	db, ok := databases[key]
+	if !ok {
+		log.Fatal("数据库配置不存在")
+	}
+	return db
+}
